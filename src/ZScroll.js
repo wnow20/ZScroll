18c99dc6b7e7f7ce6aa70ab89a24fb22f3ad0e51
wnow20 <wnow20@gmail.com>
2015-12-26 19:09:07 +0800

    finish infinite loop

--- src/ZScroll.js
+++ src/ZScroll.js
@@ -95,28 +95,52 @@
         }
 
         // 幻灯片滚动进行时参数
-
-        _.currentDirection = _.direction;
+        _.currentDirection = _.options.direction;
         _.currentOffset = 0;
         _.currentSlide = 0;
 
-        // 申明私有元素参数,在 _._buildWrap() 中被初始化
+        // 滚动参数
+        _.animating = false;
+
+        // 申明私有元素参数,在 _buildWrap() 中被初始化
         _.$list = null; // 幻灯片可展示的父容器
         _.$track = null; // 幻灯片集合路径
         _.$slides = null; // 全部的幻灯片
         _.$first = null; // 第一张幻灯片
         _.$last = null; // 最后一张幻灯片
 
+        // 申明用于无限循环的元素参数,在 _cloneSlide 中被初始化
+        _.$slidesWithCloned = null; // 所有的幻灯片,包含克隆的
+        _.$prefix = null; // 前缀克隆幻灯片
+        _.$suffix = null; // 后缀克隆幻灯片
+
         _._buildWrap();
+        _.count = _.$slides.length; // 幻灯片数量
+
+        _._setupInfinite();
         _._initPosition();
 
-        _.count = _.$slides.length; // 幻灯片数量
         _.minOffset = _._getLeft(0); // 最小偏移量
         _.maxOffset = _._getLeft(_.count - 1, true); // 最大偏移量
 
         // 初始化完毕
         _.$ele.addClass('ZScroll-initialized');
         _.isInitialized = true;
+    };
+
+    /**
+     * 获取所有幻灯片尺寸之和
+     */
+    ZScroll.prototype._getAllDim = function () {
+        var _ = this;
+        console.debug('getAllDim()');
+
+        var dim = 0;
+        _.$slides.each(function (index, slide) {
+            dim += _._getDim(slide);
+        });
+
+        return dim;
     };
 
     /**
@@ -135,6 +159,10 @@
                 .children(_.options.slideClass + ':not(.item-cloned)')
                 .addClass('ZScroll-item');
 
+        _.$slides.each(function (index, slide) {
+            $(slide).attr('data-slide-index', index);
+        });
+
         _.$first = _.$slides.first();
         _.$last = _.$slides.last();
 
@@ -144,18 +172,66 @@
             _.$slides.wrapAll('<div class="ZScroll-track"/>').parent();
 
         _.$list = _.$track.wrap('<div class="ZScroll-list"/>').parent();
+    };
 
-        _.$first
-            .clone()
-            .addClass('item-cloned')
-            .removeAttr('id')
-            .appendTo(_.$track);
+    /**
+     * 安装无限循环
+     */
+    ZScroll.prototype._setupInfinite = function () {
+        var _ = this;
+        if (!_.options.infinite) {
+            return false;
+        }
+        console.debug('_setupInfinite');
 
-        _.$last
-            .clone()
-            .addClass('item-cloned')
-            .removeAttr('id')
-            .prependTo(_.$track);
+        _._cloneSlide();
+    };
+
+    /**
+     * 克隆幻灯片,用户无限滚动
+     * @private
+     */
+    ZScroll.prototype._cloneSlide = function () {
+        var _ = this;
+        console.debug('_cloneSlide()');
+        var vpDim = _.$list[_.PROP_DIM]();
+
+
+        // 如果元素不够无需克隆
+        if (vpDim > _._getAllDim()) {
+            return false;
+        }
+
+        var pw = 0, nw = 0;
+        var slide = null;
+        var count = 0;
+        for (var i = _.count - 1; pw < vpDim; i--) {
+            slide = _.$slides.eq(i);
+            slide
+                .clone()
+                .addClass('item-cloned item-cloned-up')
+                .attr('data-slide-index', -(++count))
+                .removeAttr('id')
+                .prependTo(_.$track);
+            pw += _._getDim(slide);
+        }
+
+        count = 0;
+        for (var i = 0; nw < vpDim; i++) {
+            slide = _.$slides.eq(i);
+            slide
+                .clone()
+                .addClass('item-cloned item-cloned-down')
+                .attr('data-slide-index', _.count + (count++))
+                .removeAttr('id')
+                .appendTo(_.$track);
+
+            nw += _._getDim(slide);
+        }
+
+        _.$slidesWithCloned = _.$track.children();
+        _.$prefix = _.$track.find('.item-cloned-up');
+        _.$suffix = _.$track.find('.item-cloned-down');
     };
 
     /**
@@ -166,13 +242,13 @@
         var _ = this;
         console.debug('初始化位置 _initPosition');
 
-
         var $children = _.$track.children();
         var totalDim = 0; // 总尺寸,包括克隆的幻灯片
         $children.each(function (index, child) {
             totalDim += _._getDim(child);
         });
 
+        // TODO: bug: 当页面刚加载完的时候,有的图片还没有显示出来,过去不到真实的宽度
         _.$track.css(_.genCssDim(totalDim)); // 初始化幻灯片路径尺寸
 
         _.currentSlide = _.options.initialSlide; // 初始化当前幻灯片偏移
@@ -205,9 +281,41 @@
             dim -= (_.$list[_.PROP_DIM]() - _._getDim(_.$slides.get(index)));
         }
 
-        dim += _.$first[_.PROP_DIM]();
+        dim += _._getPrefixDim();
 
         return -dim;
+    };
+
+    /**
+     * 获取前缀克隆幻灯片的总尺寸
+     * @private
+     */
+    ZScroll.prototype._getPrefixDim = function () {
+        var _ = this;
+        console.debug('_getPrefixDim');
+
+        var dim = 0;
+        _.$prefix.each(function (index, slide) {
+            dim += _._getDim(slide);
+        });
+
+        return dim;
+    };
+
+    /**
+     * 获取后缀克隆幻灯片的总尺寸
+     * @private
+     */
+    ZScroll.prototype._getSuffixDim = function () {
+        var _ = this;
+        console.debug('_getSuffixDim');
+
+        var dim = 0;
+        _.$suffix.each(function (index, slide) {
+            dim += _._getDim(slide);
+        });
+
+        return dim;
     };
 
     /**
@@ -250,27 +358,55 @@
     ZScroll.prototype.slide = function (index) {
         var _ = this;
         console.debug('slide(%d)', index);
-
         var offset = 0;
+        var originIndex = index;
+        var isOverflow = index <0 || index >= _.count;
+        var pvp = !_.currentDirection;
 
-        if (index < 0 || index >= _.count) {
-            console.warn('指定偏移 > 幻灯片总数');
+        index = _._transforIndex(index);
+        _._prevSlide(index);
+
+        // 正在滚动则取消此次滚动行为
+        if (_.animating) {
             return false;
         }
 
-        if (!_.options.infinite && index === _.count) {
-            offset = _.maxOffset;
-        }
-
-        var pvp = !_.currentDirection;
         offset = _._getLeft(index, pvp);
 
+        if (!_.options.infinite) {
+            if (index < 0 || index >= _.count) {
+                console.warn('指定偏移 > 幻灯片总数');
+                return false;
+            }
+
+            if (index === _.count) {
+                offset = _.maxOffset;
+            }
+            if (index === 0) {
+                offset = 0;
+            }
+        }
+
+        _.animating = true;
         _.slideTo(offset, function () {
             _.currentOffset = offset;
             _.currentSlide = index;
+            _.animating =  false;
 
             _._postSlide();
         });
+    };
+
+    /**
+     * 转化偏移index
+     * @param index
+     * @private
+     */
+    ZScroll.prototype._transforIndex = function (index) {
+        while(index < 0) {
+            index += 5;
+        }
+        return index % 5;
     };
 
     /**
@@ -285,21 +421,59 @@
         _.$track.animate(_.genCssPostion(targetOffset), _.options.speed, function () {
             console.debug('animated');
 
+            callback();
         });
-
-        //_.$track.css(_.genCssPostion(targetOffset));
-
-        callback();
     };
 
     /**
      * 移动结束之后干点什么
+     * @param nextIndex 要走的下一个偏移
      * @private
      */
     ZScroll.prototype._postSlide = function () {
         var _ = this;
         console.debug('_postSlide()');
 
+    };
+
+    /**
+     * 移动开始之前要干点什么
+     * @private
+     */
+    ZScroll.prototype._prevSlide = function (nextIndex) {
+        var _ = this;
+        console.debug('_prevSlide()');
+
+        if (_.options.infinite) {
+            if (nextIndex === _.count - 1 && _.currentSlide === 0) {
+                _.exchange();
+                return true;
+            }
+            if (nextIndex === 0 && _.currentSlide === _.count - 1) {
+                _.exchange();
+                return true;
+            }
+        }
+    };
+
+    /**
+     * 始末点互换
+     */
+    ZScroll.prototype.exchange = function () {
+        var _ = this;
+        console.debug('始末点互换 exchange()');
+
+        var form = -1 * _._getPrefixDim();
+        var to = _._getLeft(_.count - 2);
+
+        if (_.currentOffset >= form) {
+            _.$track.css(_.genCssPostion(_.currentOffset - _._getAllDim()));
+            return ;
+        }
+        if (_.currentOffset <= to) {
+            _.$track.css(_.genCssPostion(_.currentOffset + _._getAllDim()));
+            return ;
+        }
     };
 
     /**
@@ -321,6 +495,7 @@
 
         _.$slides
             .removeClass('ZScroll-item')
+            .removeAttr('data-slide-index')
             .appendTo(_.$ele);
 
         _.$ele.removeClass('ZScroll-initialized');
